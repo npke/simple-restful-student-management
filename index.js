b9b6ae93a2cb7b2fef0f70ab27db74e9a8a441e2
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-09-20 11:07:39 +0700

    Complete server api with ExpressJS

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var students = [{
+    studentID: 1312241,
+    fullName: "Anh Huy Nguyen",
+    GPA: 9.0
+    },
+    {
+        studentID: 1312276,
+        fullName: "Ke Nguyen Phu",
+        GPA: 9.5
+    }];
+
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.route('/api/students')
+    .get(function (req, res) {
+        res.json(students);
+    })
+    .post(function (req, res) {
+        students.push(req.body);
+        res.json(students);
+    });
+
+app.route('/api/student/:id')
+    .get(function (req, res) {
+        res.json(students[req.params.id]);
+    })
+    .delete(function (req, res) {
+        students.splice(req.params.id, 1);
+        res.json(students);
+    })
+    .put(function (req, res) {
+        students[req.params.id] = req.body;
+        res.json(students);
+    });
+
+app.listen(8080, function () {
+    console.log('Server running at http://localhost:8080');
+});
